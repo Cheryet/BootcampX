3ef393f6be3ef621fc99d532062c4573a2537017
Cheryet <C.heryet@outlook.com>
2022-11-16 18:18:50 -0700

    sanitized values

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 })
 
-
+const cohort = [process.argv[2]]
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
 FROM assistance_requests
 JOIN teachers ON teacher_id=teachers.id
 JOIN students ON student_id=students.id
 JOIN cohorts ON cohort_id=cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 ORDER BY teacher
-`)
+`, cohort)
 .then((res) => {
   res.rows.forEach((teacher) => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`)
   })
-  
 })
